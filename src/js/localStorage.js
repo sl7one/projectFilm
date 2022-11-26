53abd12043c2a0ebe3024f69ed90a1b36e8e8b0d
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2022-11-26 12:14:13 +0200

    add Other in genres

--- src/js/localStorage.js
+++ src/js/localStorage.js
@@ -1,39 +1,50 @@
 export const localStorage = {
   //-----------------зімнні для роботи---------------------------------------------
   elementsWatched: [],
-  elementsQueue:[],
-  WATCHED_LOCAL: "list-of-films-watched",
-  QUEUE_LOCAL: "list-of-films-queue",
-  boxForFilms: document.querySelector(".gallery"),
-// --------------------додаємо в список переглянутих----------------------------
+  elementsQueue: [],
+  WATCHED_LOCAL: 'list-of-films-watched',
+  QUEUE_LOCAL: 'list-of-films-queue',
+  boxForFilms: document.querySelector('.gallery'),
+  // --------------------додаємо в список переглянутих----------------------------
   watchedList(elem) {
-
     this.elementsWatched.push(elem);
-    localStorage.setItem(this.WATCHED_LOCAL, JSON.stringify(this.elementsWatched));
-
-
+    localStorage.setItem(
+      this.WATCHED_LOCAL,
+      JSON.stringify(this.elementsWatched)
+    );
   },
   // --------------------додаємо в список в черзі-------------------------------
   queueList(elem) {
-
     this.elementsQueue.push(elem);
     localStorage.setItem(this.QUEUE_LOCAL, JSON.stringify(this.elementsQueue));
-
   },
-  checkLocal() {
-
+  checkLocalWatched() {
     // ------------------------------Перевіряємо список переглянутих----------------------------
     if (JSON.parse(localStorage.getItem(this.WATCHED_LOCAL))) {
-      this.elementsWatched.push(...JSON.parse(localStorage.getItem(this.WATCHED_LOCAL)));
-this.boxForFilms.innerHTML = (JSON.parse(localStorage.getItem(this.WATCHED_LOCAL))).join('')
+      this.elementsWatched.push(
+        ...JSON.parse(localStorage.getItem(this.WATCHED_LOCAL))
+      );
+      this.boxForFilms.innerHTML = JSON.parse(
+        localStorage.getItem(this.WATCHED_LOCAL)
+      ).join('');
+    } else {
+      this.boxForFilms.innerHTML = '';
     }
+  },
 
-    // ------------------------------Перевіряємо список в черзі----------------------------------
+  // ------------------------------Перевіряємо список в черзі----------------------------------
+
+  checkLocalQueue() {
     if (JSON.parse(localStorage.getItem(this.QUEUE_LOCAL))) {
-      this.elementsQueue.push(...JSON.parse(localStorage.getItem(this.QUEUE_LOCAL)));
-      this.boxForFilms.insertAdjacentHTML("beforeend",(JSON.parse(localStorage.getItem(this.WATCHED_LOCAL))).join(''))
+      this.elementsQueue.push(
+        ...JSON.parse(localStorage.getItem(this.QUEUE_LOCAL))
+      );
+      this.boxForFilms.insertAdjacentHTML(
+        'beforeend',
+        JSON.parse(localStorage.getItem(this.WATCHED_LOCAL)).join('')
+      );
+    } else {
+      this.boxForFilms.innerHTML = '';
     }
-    //------------------------Якщо локад пустий - пуста секція------------------------------------
-    this.boxForFilms.innerHTML = ""
   },
 };
