f37df359042811f117ced7d9e4b9f7eb84987f5f
Slava <110696118+sl7one@users.noreply.github.com>
2022-11-27 21:49:45 +0200

    upd

---
+++ src/js/library.js
@@ -0,0 +1,7 @@
+import { ref } from './js/ref';
+import { markup } from './js/markup';
+import { render } from './js/render';
+import { request } from './js/requestAPI';
+import { shema } from './js/shema';
+import { refsModal, showModal, hideModal, modalMovie } from './js/modalMovie';
+import { localStorageList } from './js/localStorage';
