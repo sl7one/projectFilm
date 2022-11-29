4053db1b475c3cb9c85008c8d8b1c2598daf9b8d
Slava <110696118+sl7one@users.noreply.github.com>
2022-11-30 00:53:57 +0200

    Update modalMovie.js

--- src/js/modalMovie.js
+++ src/js/modalMovie.js
@@ -1,10 +1,9 @@
 import { request, API_KEY } from './requestAPI';
 
 export const refsModal = {
-
-  mainContainer: document.querySelector(".main.container"),
+  mainContainer: document.querySelector('.main.container'),
   backdrop: document.querySelector('[data-modal]'),
-  modal: document.querySelector("div.modal-movie-info"),
+  modal: document.querySelector('div.modal-movie-info'),
   closeModalBtn: document.querySelector('[data-modal-close]'),
 
   poster: document.querySelector('#poster__image'),
@@ -19,27 +18,29 @@ export const refsModal = {
   btnQueue: document.querySelector('#add-to-queue'),
 };
 
-let offsetX = 0; 
+let offsetX = 0;
 let offsetY = 0;
 
-refsModal.closeModalBtn.addEventListener("click", hideModal);
-document.addEventListener("keydown", (event) => {
-  if (event.code === "Escape") { hideModal() }
-} )
-document.addEventListener("click", (event)=> {
-  if (event.target.className === "backdrop") { hideModal() }
-
-})
+refsModal.closeModalBtn.addEventListener('click', hideModal);
+document.addEventListener('keydown', event => {
+  if (event.code === 'Escape') {
+    hideModal();
+  }
+});
+document.addEventListener('click', event => {
+  if (event.target.className === 'backdrop') {
+    hideModal();
+  }
+});
 
 export function showModal() {
-
   offsetX = window.pageXOffset;
   offsetY = window.pageYOffset;
 
   const body = document.body;
-  refsModal.mainContainer.style.overflow = "hidden";
-  body.style.overflow = "hidden";
-  refsModal.modal.overflowY = "scroll";
+  refsModal.mainContainer.style.overflow = 'hidden';
+  body.style.overflow = 'hidden';
+  refsModal.modal.overflowY = 'scroll';
   //window.scrollTo(offsetX, offsetY);
   refsModal.backdrop.classList.remove('is-hidden');
 
@@ -50,21 +51,18 @@ export function showModal() {
 }
 
 export function hideModal() {
-  
   refsModal.backdrop.classList.add('is-hidden');
   const body = document.body;
-  body.style.overflow = "";
+  body.style.overflow = '';
 
-  
-//  refsModal.closeModalBtn.removeEventListener("click", hideModal);
+  //  refsModal.closeModalBtn.removeEventListener("click", hideModal);
 }
 
 export async function modalMovie(event) {
-  
   showModal();
 
   let movieCard = event.target.parentNode;
-  
+
   while (
     movieCard.nodeName !== 'LI' &&
     movieCard.className !== 'gallery__item'
@@ -79,16 +77,15 @@ export async function modalMovie(event) {
     `https://image.tmdb.org/t/p/w500${data?.poster_path}?api_key=${API_KEY}&language=en-US` ??
     '#';
 
-  refsModal.movieName.textContent = 
-    data?.original_title ?? 
-    data?.original_name ?? 'NAMELESS MOVIE';
-  refsModal.vote.textContent = data?.vote_average ?? 0;
+  refsModal.movieName.textContent =
+    data?.original_title ?? data?.original_name ?? 'NAMELESS MOVIE';
+  refsModal.vote.textContent = data?.vote_average.toFixed(1) ?? 0;
   refsModal.votes.textContent = '/ ' + data?.vote_count ?? 0;
-  refsModal.popularity.textContent = data?.popularity ?? 0;
-  refsModal.originalTitle.textContent = data?.original_title ?? '';
+  refsModal.popularity.textContent = data?.popularity.toFixed(1) ?? 0;
+  refsModal.originalTitle.textContent =
+    data?.original_title.toUpperCase() ?? '';
   refsModal.genre.textContent =
     data?.genres.map(genre => genre?.name).join(', ') ?? 'Not set';
   refsModal.about.textContent =
     data?.overview ?? 'There is no information provided.';
-    
 }
